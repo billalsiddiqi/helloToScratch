99a841adcbe393108932ea4ed93a7968d710fec9
Khwaja Billal Siddiqi <52275107+billalsiddiqi@users.noreply.github.com>
2024-02-15 11:47:25 +0430

    Update index.js

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -7,43 +7,23 @@ class ScratchFetch {
             "id": "Hello",
             "name": "Hello",
             "blocks": [
-                    {
-                        // function where your code logic lives
-                        opcode: 'myFirstBlock',
-                
-                        // type of block
-                        blockType: BlockType.REPORTER,
-                
-                        // label to display on the block
-                        text: 'Title for ISBN book [BOOK_NUMBER]',
-                
-                        // arguments used in the block
-                        arguments: {
-                          BOOK_NUMBER: {
-                            defaultValue: 1718500564,
-                
-                            // type/shape of the parameter
-                            type: ArgumentType.NUMBER
-                          }
+                   {
+                        "opcode": "jsonExtract",
+                        "blockType": "reporter",
+                        "text": "extract [name] from [data]",
+                        "arguments": {
+                            "name": {
+                                "type": "string",
+                                "defaultValue": "temperature"
+                            },
+                            "data": {
+                                "type": "string",
+                                "defaultValue": '{"temperature": 12.3}'
+                            },
                         }
-                      }
+                    },
                 ],
         };
-    }
-    
-     myFirstBlock ({ BOOK_NUMBER }) {
-      return fetch('https://openlibrary.org/isbn/' + BOOK_NUMBER + '.json')
-        .then((response) => {
-          if (response.ok) {
-            return response.json();
-          }
-          else {
-            return { title: 'Unknown' };
-          }
-        })
-        .then((bookinfo) => {
-          return bookinfo.title;
-        });
     }
 
 }
